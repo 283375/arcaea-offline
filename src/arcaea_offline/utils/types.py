5e13685cf49fc36b5445acc1b844c75b29f7b918
283375 <bilibili283375@163.com>
2023-06-06 20:01:53 +0800

    style: pre-commit

--- src/arcaea_offline/utils/types.py
+++ src/arcaea_offline/utils/types.py
@@ -1,6 +1,8 @@
-from typing import Any, Protocol, ClassVar, Dict
+from typing import Any, ClassVar, Dict, Protocol
+
 
 class TDataclass(Protocol):
     __dataclass_fields__: ClassVar[Dict]
+
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         ...
