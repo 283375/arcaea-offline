7c000d01cbd34c669f90b4f54da6467826b6c2a9
283375 <bilibili283375@163.com>
2023-10-12 16:21:37 +0800

    feat(utils): text converters

--- src/arcaea_offline/utils/score.py
+++ src/arcaea_offline/utils/score.py
@@ -2,6 +2,15 @@ from typing import Any, Sequence
 
 SCORE_GRADE_FLOOR = [9900000, 9800000, 9500000, 9200000, 8900000, 8600000, 0]
 SCORE_GRADE_TEXTS = ["EX+", "EX", "AA", "A", "B", "C", "D"]
+MODIFIER_TEXTS = ["NORMAL", "EASY", "HARD"]
+CLEAR_TYPE_TEXTS = [
+    "TRACK LOST",
+    "NORMAL CLEAR",
+    "FULL RECALL",
+    "PURE MEMORY",
+    "EASY CLEAR",
+    "HARD CLEAR",
+]
 
 
 def zip_score_grade(score: int, __seq: Sequence, default: Any = "__PRESERVE__"):
@@ -27,3 +36,11 @@ def zip_score_grade(score: int, __seq: Sequence, default: Any = "__PRESERVE__"):
 
 def score_to_grade_text(score: int) -> str:
     return zip_score_grade(score, SCORE_GRADE_TEXTS)
+
+
+def modifier_to_text(modifier: int) -> str:
+    return MODIFIER_TEXTS[modifier]
+
+
+def clear_type_to_text(clear_type: int) -> str:
+    return CLEAR_TYPE_TEXTS[clear_type]
