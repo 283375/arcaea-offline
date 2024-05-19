a6d71135fb7c9ff9189dd3c84721950eed9f349b
283375 <bilibili283375@163.com>
2024-05-20 00:22:12 +0800

    feat: ArcaeaLanguage enum

--- src/arcaea_offline/constants/enums/__init__.py
+++ src/arcaea_offline/constants/enums/__init__.py
@@ -1,3 +1,11 @@
 from .clear_type import ArcaeaPlayResultClearType
+from .language import ArcaeaLanguage
 from .modifier import ArcaeaPlayResultModifier
 from .rating_class import ArcaeaRatingClass
+
+__all__ = [
+    "ArcaeaPlayResultClearType",
+    "ArcaeaPlayResultModifier",
+    "ArcaeaRatingClass",
+    "ArcaeaLanguage",
+]
---
+++ src/arcaea_offline/constants/enums/language.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ArcaeaLanguage(Enum):
+    JA = "ja"
+    KO = "ko"
+    ZH_HANT = "zh-Hant"
+    ZH_HANS = "zh-Hans"
