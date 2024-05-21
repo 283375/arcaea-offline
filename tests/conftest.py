a27afca8a7f7b8b68e38fd9feda707c80f83c8d1
283375 <bilibili283375@163.com>
2024-05-21 21:01:35 +0800

    test: conftest database clean-up

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,53 @@
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 # region sqlalchemy fixtures
-# from https://medium.com/@vittorio.camisa/agile-database-integration-tests-with-python-sqlalchemy-and-factory-boy-6824e8fe33a1
 engine = create_engine("sqlite:///:memory:")
 Session = sessionmaker()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def db_conn():
-    connection = engine.connect()
-    yield connection
-    connection.close()
+    conn = engine.connect()
+    yield conn
+    conn.close()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture()
 def db_session(db_conn):
-    transaction = db_conn.begin()
     session = Session(bind=db_conn)
     yield session
     session.close()
-    transaction.rollback()
+
+    # drop everything
+    query_tables = db_conn.execute(
+        text("SELECT name FROM sqlite_master WHERE type='table'")
+    ).fetchall()
+    for row in query_tables:
+        table_name = row[0]
+        db_conn.execute(text(f"DROP TABLE {table_name}"))
+
+    query_views = db_conn.execute(
+        text("SELECT name FROM sqlite_master WHERE type='view'")
+    ).fetchall()
+    for row in query_views:
+        view_name = row[0]
+        db_conn.execute(text(f"DROP VIEW {view_name}"))
+
+    query_indexes = db_conn.execute(
+        text("SELECT name FROM sqlite_master WHERE type='index'")
+    ).fetchall()
+    for row in query_indexes:
+        index_name = row[0]
+        db_conn.execute(text(f"DROP INDEX {index_name}"))
+
+    query_triggers = db_conn.execute(
+        text("SELECT name FROM sqlite_master WHERE type='trigger'")
+    ).fetchall()
+    for row in query_triggers:
+        trigger_name = row[0]
+        db_conn.execute(text(f"DROP TRIGGER {trigger_name}"))
 
 
 # endregion
