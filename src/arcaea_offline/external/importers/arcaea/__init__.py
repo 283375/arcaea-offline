5e996d35d2a88e602e9c163a342a900150a7f7b9
283375 <bilibili283375@163.com>
2024-09-27 23:12:58 +0800

    chore!: remove legacy external arcaea parsers

---
+++ src/arcaea_offline/external/importers/arcaea/__init__.py
@@ -0,0 +1,10 @@
+from .lists import ArcaeaPacklistParser, ArcaeaSonglistParser
+from .online import ArcaeaOnlineApiParser
+from .st3 import ArcaeaSt3Parser
+
+__all__ = [
+    "ArcaeaPacklistParser",
+    "ArcaeaSonglistParser",
+    "ArcaeaOnlineApiParser",
+    "ArcaeaSt3Parser",
+]
