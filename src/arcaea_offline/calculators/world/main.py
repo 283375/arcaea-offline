2377d233b1bf9904dbbff67ec181cbbe6f01bce4
283375 <bilibili283375@163.com>
2024-10-01 22:57:19 +0800

    chore: code reformatting

--- src/arcaea_offline/calculators/world/main.py
+++ src/arcaea_offline/calculators/world/main.py
@@ -52,6 +52,4 @@ class WorldMainMapCalculators:
         play_rating_sqrt = (
             Decimal(50) * step - Decimal("2.5") * partner_step_value
         ) / (Decimal("2.45") * partner_step_value)
-        return (
-            play_rating_sqrt**2 if play_rating_sqrt >= 0 else -(play_rating_sqrt**2)
-        )
+        return play_rating_sqrt**2 if play_rating_sqrt >= 0 else -(play_rating_sqrt**2)
